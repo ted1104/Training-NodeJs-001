65fcba6b660c5352ce45e5714bfc046c8772ad1d
Teddy Walter <teddywalter2016@gmail.com>
2022-02-13 00:16:11 +0200

    intro node and global variable

---
+++ app.js
@@ -0,0 +1,9 @@
+// GLOBALS - NO WINDOWS
+/*
+__dirname - path to current path
+__filename - file name of the current file
+require : function to use modules (commonJS)
+module : info about current module (file)
+process : info about env where the program is beign executing
+*/
+console.log(__dirname, __filename);
