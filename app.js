76c23a6c2a4627c360090c73754c301a7bc6e3e4
Teddy Walter <teddywalter2016@gmail.com>
2022-02-24 00:36:32 +0200

    npm commande, init project, init -y, install packagelcaly and globaly, install package for dev

--- app.js
+++ app.js
@@ -7,8 +7,19 @@
   npm install -g <packageName>
   sudo npm install -g <packageName> (mac)
 
+  install package uniquement pour l'usage du developpement
+  npm install <packageName> -D / npm i <packageName> --save-dev
+
   package.json : manifest file (stores important informations about project and packages used)
   manual approach we need to create package.json in the root,
   npm init (step by step, press enter to skip)
   npm init -y (initialisatio project with all config in default)
+
+  quand on ajoute des script dans package.json ex : dev ou autre, on execute avec :
+  npm run dev
+
+  pour desinstaller le package 
+  npm uninstall <packageName>
+  
+
 */
